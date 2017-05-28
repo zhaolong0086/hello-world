b257bbad28103b961c891fa923be234d07c4ed62
zhaolong0086 <tsinly@163.com>
2017-05-28 14:06:36 +0800

    https://github.com/zhaolong0086/hello-world.git

---
+++ DataBinding/Clerk.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace WpfLearning
+{
+    public class Clerk : INotifyPropertyChanged
+    {
+        public Clerk()
+        {
+        }
+
+        public Clerk(string name, string surName, string sex)
+        {
+            Name = name;
+            SurName = surName;
+            Sex = sex;
+        }
+
+        /// <summary>名</summary>
+        public string Name { get { return name; } set { name = value; IChanged("Name"); } }
+        string name;
+
+        /// <summary>姓</summary>
+        public string SurName { get { return surName; } set { surName = value; IChanged("SurName"); } }
+        string surName;
+
+        /// <summary>性别</summary>
+        public string Sex { get { return sex; } set { sex = value; IChanged("Sex"); } }
+        string sex;
+
+        /// <summary>地址</summary>
+        public string Address { get { return address; } set { address = value; IChanged("Address"); } }
+        string address;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void IChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ DataBinding/ClerkManagerViewModel.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+using WpfLearning;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace WpfLearning
+{
+    class ClerkManagerViewModel : INotifyPropertyChanged
+    {
+        public ClerkManagerViewModel()
+        {
+            ClerkList.Add(new Clerk("long", "zhao", "male"));
+            ClerkList.Add(new Clerk("si", "li", "female"));
+        }
+
+
+        public ObservableCollection<Clerk> ClerkList { get { return clerkList; } set { clerkList = value; IChanged("ClerkList"); } }
+        ObservableCollection<Clerk> clerkList = new ObservableCollection<Clerk>();
+
+        /// <summary></summary>
+        public Clerk CurrentClerk
+        {
+            get { return currentClerk; }
+            set { currentClerk = value; IChanged("CurrentClerk"); }
+        }
+        private Clerk currentClerk = null;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void IChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+
+
+
+
+
+        public ICommand AddNewItem
+        {
+            get { return new CommandBase(AddNewItemExecute, obj => { return true; }); }
+        }
+
+        public void AddNewItemExecute(object sender)
+        {
+            Clerk clk = new Clerk("NewName", "NewSurName", "Sex");
+            ClerkList.Add(clk);
+        }
+
+        public ICommand DeleteItem
+        {
+            get { return new CommandBase(DeleteItemExecute, obj => { return true; }); }
+        }
+
+        public void DeleteItemExecute(object sender)
+        {
+            if (CurrentClerk != null)
+            {
+                ClerkList.Remove(CurrentClerk);
+                CurrentClerk = null;
+            }
+        }
+
+        public ICommand SaveToFile
+        {
+            get { return new CommandBase(SaveToFileExecute, obj => { return true; }); }
+        }
+
+        public void SaveToFileExecute(object sender)
+        {
+            string sPath = @"C:\Users\long\Documents\Visual Studio 2013\Projects\WpfLearning\DataBinding\Data.xml";
+            if (File.Exists(sPath))
+            { 
+            }
+
+            FileStream stream = new FileStream(sPath, FileMode.OpenOrCreate);
+            XmlSerializer xmlSer = new XmlSerializer(typeof(ObservableCollection<Clerk>));
+            xmlSer.Serialize(stream, ClerkList);
+            stream.Close();
+
+        }
+
+
+        public ICommand LoadFromFile
+        {
+            get { return new CommandBase(LoadFromFileExecute, obj => { return true; }); }
+        }
+
+        public void LoadFromFileExecute(object sender)
+        {
+        }
+
+
+    }
+}
